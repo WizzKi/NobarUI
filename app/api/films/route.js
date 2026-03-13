3f35dddf7193febe75f258c1168fb116c392479d
WizzKi <gamingarisky@gmail.com>
2026-03-14 00:25:10 +0700

    add baruuu

--- app/api/films/route.js
+++ app/api/films/route.js
@@ -1,26 +1,37 @@
 import { prisma } from "@/lib/prisma"
 
-export async function GET() {
+export async function GET(){
 
-  const films = await prisma.film.findMany({
-    orderBy:{
-      createdAt:"desc"
-    }
-  })
+const films = await prisma.film.findMany({
+orderBy:{
+id:"desc"
+}
+})
 
-  return Response.json(films)
+return Response.json(films)
+
 }
 
-export async function POST(req:Request){
+export async function POST(req){
 
-  const body = await req.json()
+const body = await req.json()
 
-  const film = await prisma.film.create({
-    data:{
-      ...body,
-      id: BigInt(body.id)
-    }
-  })
+const film = await prisma.film.create({
+data:{
+title: body.title,
+genre: body.genre,
+date: body.date,
+comment: body.comment,
+poster: body.poster,
 
-  return Response.json(film)
+riskiRating: body.riskiRating,
+rapaRating: body.rapaRating,
+
+riskiSad: body.riskiSad,
+rapaSad: body.rapaSad
+}
+})
+
+return Response.json(film)
+
 }
